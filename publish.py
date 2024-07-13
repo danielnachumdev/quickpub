5cb730b09eaf5182d6931383f63fe4783b858a43
danielnachumdev <danielnachumdev@gmail.com>
2024-07-13 16:55:11 +0300

    re-enabled additional checks
    Took 34 seconds

--- publish.py
+++ publish.py
@@ -12,12 +12,12 @@ def main() -> None:
         dependencies=["twine", "danielutils"],
         min_python="3.8.0",
         config=AdditionalConfiguration(
-            # python_manager=CondaPythonManager(["base", "390", "380"]),
-            # runners=[
-            #     MypyRunner(bound="<15", configuration_path="./mypy.ini"),
-            #     PylintRunner(bound=">=0.8", configuration_path="./.pylintrc"),
-            #     UnittestRunner(bound=">=0.8"),
-            # ]
+            python_manager=CondaPythonManager(["base", "390", "380"]),
+            runners=[
+                MypyRunner(bound="<15", configuration_path="./mypy.ini"),
+                PylintRunner(bound=">=0.8", configuration_path="./.pylintrc"),
+                UnittestRunner(bound=">=0.8"),
+            ]
         )
     )
 
