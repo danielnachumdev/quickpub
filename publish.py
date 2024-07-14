de8c947b63b3b2f301d85dbdedac50631d7549e3
danielnachumdev <danielnachumdev@gmail.com>
2024-07-14 23:37:10 +0300

    fixed back publish.py
    Took 16 minutes

--- publish.py
+++ publish.py
@@ -13,14 +13,12 @@ def main() -> None:
         upload_targets=[PypircUploadTarget(), GithubUploadTarget()],
         python_interpreter_provider=CondaPythonProvider(["base", "390", "380"]),
         quality_assurance_runners=[
-            PytestRunner(target=".\\pytest_tests_demo\\")
-            # MypyRunner(bound="<=15", configuration_path="./mypy.ini"),
-            # PylintRunner(bound=">=0.8", configuration_path="./.pylintrc"),
-            # UnittestRunner(bound=">=0.8"),
+            MypyRunner(bound="<=15", configuration_path="./mypy.ini"),
+            PylintRunner(bound=">=0.8", configuration_path="./.pylintrc"),
+            UnittestRunner(bound=">=0.8"),
         ],
         dependencies=["danielutils>=0.9.90"],
         min_python="3.8.0",
-        demo=True
     )
 
 
