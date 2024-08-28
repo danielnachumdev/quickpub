1b69c95e3a186365c0f0a2dba89c9154d3406caf
danielnachumdev <danielnachumdev@gmail.com>
2024-08-28 16:03:44 +0300

    updated to version 2.0.3
    Took 20 minutes

--- publish.py
+++ publish.py
@@ -17,7 +17,7 @@ def main() -> None:
         ],
         build_schemas=[SetuptoolsBuildSchema()],
         upload_targets=[PypircUploadTarget(), GithubUploadTarget()],
-        python_interpreter_provider=CondaPythonProvider(["base", "390", "380"]),
+        python_interpreter_provider=CondaPythonProvider(["base", "39", "380"]),
         global_quality_assurance_runners=[
             MypyRunner(bound="<=20", configuration_path="./mypy.ini"),
             PylintRunner(bound=">=0.8", configuration_path="./.pylintrc"),
