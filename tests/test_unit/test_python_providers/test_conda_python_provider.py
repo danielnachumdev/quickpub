bb221ea0146196097a8cbf96aa5fd736a767cc19
danielnachumdev <danielnachumdev@gmail.com>
2024-08-28 12:43:37 +0300

    made test X2 times faster
    Took 24 minutes

--- tests/test_unit/test_python_providers/test_conda_python_provider.py
+++ tests/test_unit/test_python_providers/test_conda_python_provider.py
@@ -1,21 +1,48 @@
+import asyncio
 import unittest
+from concurrent.futures import ThreadPoolExecutor
+from typing import Coroutine, Optional
+
 from quickpub import CondaPythonProvider
 
 
-class TestCondaPythonProvider(unittest.TestCase):
-    def test_all_envs_should_succeed(self):
+def run_async(coroutine: Coroutine) -> Optional[Exception]:
+    loop = asyncio.new_event_loop()
+    asyncio.set_event_loop(loop)
+    try:
+        loop.run_until_complete(coroutine)
+    except Exception as e:
+        return e
+    finally:
+        loop.close()
+    return None
+
+
+class TestCondaPythonProvider(unittest.IsolatedAsyncioTestCase):
+    async def test_all_envs_should_succeed(self):
         envs = CondaPythonProvider.get_available_envs()
         provider = CondaPythonProvider(list(envs))
-        for env_name, executor in provider:
-            expected_index: int = 0
-            with executor:
-                code, stdout, stderr = executor.execute("conda info")
-            self.assertEqual(0, code)
-            self.assertTrue(len(stdout) > 0, "stdout should not be empty")
-            self.assertTrue(len(stderr) == 0, "stderr should be empty")
-            if stdout[0] == '\x1b[0m\n':
-                expected_index: int = 1
+        futures = []
+        with ThreadPoolExecutor() as pool:
+            for i, tup in enumerate(provider):
+                async def wrapper(env_name, executor) -> None:
+                    expected_index: int = 0
+                    with executor:
+                        code, stdout, stderr = executor.execute("conda info")
+                    self.assertEqual(0, code)
+                    self.assertTrue(len(stdout) > 0, "stdout should not be empty")
+                    self.assertTrue(len(stderr) == 0, "stderr should be empty")
+                    if stdout[0] == '\x1b[0m\n':
+                        expected_index: int = 1
 
-            indicator_line = stdout[expected_index].strip()
-            current_env = indicator_line.split(' ')[-1]
-            self.assertIn(current_env, envs)
+                    indicator_line = stdout[expected_index].strip()
+                    current_env = indicator_line.split(' ')[-1]
+                    self.assertIn(current_env, envs)
+
+                env_name, executor = tup
+                coro = wrapper(env_name, executor)
+                futures.append(pool.submit(run_async, coro))
+            for future in futures:
+                exception = future.result()
+                if exception:
+                    raise exception
