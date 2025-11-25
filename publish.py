e832364068fa1c1a41bd091b2c147b85f6e1cb72
danielnachumdev <danielnachumdev@gmail.com>
2025-11-25 22:29:35 +0200

    updated descirption
    Took 1 minute

--- publish.py
+++ publish.py
@@ -11,7 +11,7 @@ def main() -> None:
         version="3.0.61",
         author="danielnachumdev",
         author_email="danielnachumdev@gmail.com",
-        description="A python package to quickly configure and publish a new package",
+        description="A local CI/CD simulation tool that runs quality checks, tests, and validations locally before publishing Python packages, ensuring higher build pass rates and faster feedback loops",
         homepage="https://github.com/danielnachumdev/quickpub",
         enforcers=[
             PypircEnforcer(), ReadmeEnforcer(), LicenseEnforcer(),
@@ -19,7 +19,8 @@ def main() -> None:
         ],
         build_schemas=[SetuptoolsBuildSchema()],
         upload_targets=[PypircUploadTarget(), GithubUploadTarget()],
-        python_interpreter_provider=CondaPythonProvider(["base", "390", "380"]),
+        python_interpreter_provider=CondaPythonProvider(
+            ["base", "390", "380"]),
         global_quality_assurance_runners=[
             MypyRunner(bound="<=20", configuration_path="./mypy.ini"),
             PylintRunner(bound=">=0.8", configuration_path="./.pylintrc"),
